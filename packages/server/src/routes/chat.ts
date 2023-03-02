558cb7dbff9adcdd8f5d1371587e376a9620b1fa
seonhwa <125868397+miaaK@users.noreply.github.com>
2023-03-02 14:36:47 +0900

    feat: add router for chat and room
    ~chat
    - chat list
    - send messages
    
    ~room
    - lists of chat rooms
    - details of chat rooms
    - create new chat rooms

---
+++ packages/server/src/routes/chat.ts
@@ -0,0 +1,41 @@
+import { Router } from 'express';
+
+import Chat from '../schemas/chat';
+import Room from '../schemas/room';
+import User from '../schemas/user';
+
+const router = Router();
+
+/* Chat list */
+router.get('/:roomId', async(req, res) => {
+    try {
+        const chat = await Chat.findAll({
+            where: {
+                roomId: req.params.roomId
+            },
+            include: [ User, Room ]  
+        });
+        res.json(chat);
+    } catch (e) {
+
+    }
+})
+
+
+
+/* Send messages */
+router.get('/:roomId', async(req, res) => {
+    try {
+        const chat = await Chat.create({
+            senderId: req.session.userId,
+            content: req.body.content,
+            roomId: req.params.roomId
+        });
+      /* TODO:: socket */
+      res.json({ message:'OK' });
+    } catch (e) {
+
+    }
+})
+
+export default router;
