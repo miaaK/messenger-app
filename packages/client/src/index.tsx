a76cda50d5eb8d45348926f873c9918aba2318e5
seonhwa <125868397+miaaK@users.noreply.github.com>
2023-03-06 15:53:25 +0900

    feat: add bottom navigation bar

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import theme from "./theme";
+
+import { ThemeProvider } from "@emotion/react";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
@@ -12,7 +15,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- packages/client/src/pages/Lobby.tsx
+++ packages/client/src/pages/Lobby.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import BottomNavigation from "../components/BottomNavigation";
 import TopNavigation from "../components/TopNavigation";
 
 const Lobby: React.FC = () => {
   return <div>
     <TopNavigation title="Lobby" />
+    <BottomNavigation />
   </div>;
 };
 
