a82e993ad239dbc5b044c2633b2ac8849450ad49
seonhwa <125868397+miaaK@users.noreply.github.com>
2023-03-06 14:46:29 +0900

    feat: add pages and router
    - Lobby
    - Friends
    - RoomList
    - RoomDetail
    - SeeMore

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Lobby from './pages/Lobby';
+import Friends from './pages/Friends';
+import RoomList from './pages/RoomList';
+import RoomDetail from './pages/RoomDetail';
+import SeeMore from './pages/SeeMore';
 
 function App() {
   return (
-    <div>Welcome !</div>
+    <Routes>
+      <Route index element={<Lobby />} /> {/* Lobby */}
+      <Route path='/friends' element={<Friends />} /> {/* Friends */}
+      <Route path='/rooms' element={<RoomList />} /> {/* RoomList */}
+      <Route path='/rooms/:roomId' element={<RoomDetail />} /> {/* RoomDetails */}
+      <Route path='/more' element={<SeeMore />} /> {/* SeeMore */}
+    </Routes>
   );
 }
 
